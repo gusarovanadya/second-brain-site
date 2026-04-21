0653d1dfad92a298c47bf721db03e1e9d6222f49
gusarovanadya <gusarovanadyapr@gmail.com>
2026-04-21 17:24:11 +0300

    Delete netlify directory

--- netlify/functions/tbank-notify.mjs
+++
@@ -1,9 +0,0 @@
-export default async (req) => {
-  const body = await req.text();
-  console.log("T-Bank notification:", body);
-
-  return new Response("OK", {
-    status: 200,
-    headers: { "Content-Type": "text/plain" }
-  });
-};
